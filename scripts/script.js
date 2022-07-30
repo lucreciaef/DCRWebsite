3a1b79d88a00f2788e6b8e69d94e688b5f0548a6
lucreciaef <lucrecia.e.f@gmail.com>
2022-07-30 16:06:09 +0200

    working nav menu, responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
+	// Using templated header & footer
+	// Source: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_templates_and_slots
+	let headerTemplate = document.getElementById("header-template");
+	let footerTemplate = document.getElementById("footer-template");
+	let headerTemplateContent = headerTemplate.content;
 
-// Using templated header & footer
-// Source: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_templates_and_slots
-let headerTemplate = document.getElementById("header-template");
-let footerTemplate = document.getElementById("footer-template");
+	let footerTemplateContent = footerTemplate.content;
 
-let headerTemplateContent = headerTemplate.content;
-let footerTemplateContent = footerTemplate.content;
+	document.getElementById("header-placeholder").appendChild(headerTemplateContent);
+	document.body.appendChild(footerTemplateContent);
 
-document.getElementById("header-placeholder").appendChild(headerTemplateContent);
-document.body.appendChild(footerTemplateContent);
 
-function openHamburgerMenu() {
-	let x = document.getElementById("nav-menu-links");
+function clickOnMenuButton() {
+	let x = document.getElementById("nav-menu-id");
 	console.log("print x")
-	if (x.className === "navigation-menu nav-menu-links") {
+	if (x.className === "nav-bar-links") {
 		console.log("test 1")
 		x.className += " responsive";
 	} else {
-		x.className = "navigation-menu nav-menu-links";
+		x.className = "nav-bar-links";
 		console.log("test 2")
 	}
 }
